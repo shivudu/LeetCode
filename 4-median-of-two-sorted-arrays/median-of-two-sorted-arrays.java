4cbfedbfe4e9a107e0dc5dcdb204c2981c17b5f3
shivudu <shivudu.s@gmail.com>
2024-04-15 14:27:18 +0530

    Time: 2 ms (64.42%) | Memory: 45.8 MB (75.65%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,29 +1,57 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i = 0, j =0, n=0;
-        int[] nums = new int[nums1.length + nums2.length];
+        int i = 0, j =0, n=0, ind1=0, ind2=0;
+        int f=0, s=0;
+        int m = nums1.length + nums2.length;
+        if(m % 2 == 0){
+            ind1 = m/2-1;
+            ind2 = m/2;
+        }
+        else{
+            ind1 = m/2;
+        }
         while(i < nums1.length && j < nums2.length){
             if(nums1[i] < nums2[j]){
-                nums[n++] = nums1[i++];
+               int tmp = nums1[i++];
+                if(n == ind1)
+                    f = tmp;
+                if(n == ind2)
+                    s = tmp;
+                n++;
             }
             else{
-                nums[n++] = nums2[j++];
+                int tmp = nums2[j++];
+                if(n == ind1)
+                    f = tmp;
+                if(n == ind2)
+                    s = tmp;
+                n++;
             }
         }
-        if(i!= nums1.length){
-            while(i < nums1.length){
-                nums[n++] = nums1[i++];
-            }
-        }
-        if(j!= nums2.length){
-            while(j < nums2.length){
-                nums[n++] = nums2[j++];
-            }
-        }
-        if(nums.length %2 != 0){
-            return nums[nums.length/2];
+        if(i == nums1.length)
+                while(j < nums2.length){
+                    int tmp = nums2[j++];
+                if(n == ind1)
+                    f = tmp;
+                if(n == ind2)
+                    s = tmp;
+                n++;
+                }
+            if(j == nums2.length)
+                while(i < nums1.length){
+                    int tmp = nums1[i++];
+                if(n == ind1)
+                    f = tmp;
+                if(n == ind2)
+                    s = tmp;
+                n++;
+                }
+
+        
+        if(m %2 != 0){
+            return f;
         }
         else
-            return ((double)nums[nums.length/2 -1] + nums[nums.length/2])/2;
+            return ((double)f+s)/2;
     }
 }
