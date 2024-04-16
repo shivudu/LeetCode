0ade12fdd0e63ddede8e62c1fa2ca11656057ac2
shivudu <shivudu.s@gmail.com>
2024-04-16 20:31:21 +0530

    Time: 7 ms (93.98%) | Memory: 45.4 MB (74.70%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,12 +1,12 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
-        PriorityQueue<int[]> q = new PriorityQueue<>((a,b)->b[0]-a[0]);
+        //PriorityQueue<int[]> q = new PriorityQueue<>((a,b)->b[0]-a[0]);
+        TreeSet<int[]> s = new TreeSet<>((a,b)->b[0]-a[0]);
         for(int i=0;i<score.length;i++)
-            q.add(new int[]{score[i],i});
+            s.add(new int[]{score[i],i});
         String[] res = new String[score.length];
         int i = 1;
-        while(!q.isEmpty()){
-            int[] t = q.poll();
+        for(int[] t: s){
             if(i==1)
              res[t[1]] = "Gold Medal";
             else if(i==2)
