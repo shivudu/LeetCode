e67c687d2e8a997be94534e6a7eb5d134973877f
shivudu <shivudu.s@gmail.com>
2024-04-18 19:03:43 +0530

    Time: 5 ms (6.59%) | Memory: 66.7 MB (5.18%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,14 +1,15 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] res =new int[nums.length];
-        backtrack(nums,1,0,res);
-        return res;
+        //int[] res =new int[nums.length];
+        backtrack(nums,1,0);
+        return nums;
     }
-    public int backtrack(int[] nums,int prod, int i,int[] res){
+    public int backtrack(int[] nums,int prod, int i){
         if(i == nums.length)
             return 1;
-        int ri= backtrack(nums, nums[i]*prod, i+1,res);
-        res[i] = ri * prod;
-        return ri * nums[i];
+        int ri= backtrack(nums, nums[i]*prod, i+1);
+        int t = nums[i];
+        nums[i] = ri * prod;
+        return ri * t;
     }
 }
