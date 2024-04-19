4b2b07ed9a96af5a11e1639dd67157809ab5dda4
shivudu <shivudu.s@gmail.com>
2024-04-19 15:00:17 +0530

    Time: 2 ms (65.12%) | Memory: 41.6 MB (26.47%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int h = 0;
+        for(int i=citations.length-1;i>=0;i--){
+            if(citations[i] > h)
+                h++;
+        }
+        return h;
+    }
+}
