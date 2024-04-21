2e29ad5d901b14196880280246b6743f4cca88b3
shivudu <shivudu.s@gmail.com>
2024-04-21 15:33:33 +0530

    Time: 207 ms (6.49%) | Memory: 45.6 MB (5.60%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,50 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] rom = new int[]{1000, 500, 100, 50, 10, 5, 1};
+        int i = 0;
+        String res = "";
+        while(num > 0){
+            int rem = num % rom[i];
+            System.out.println("num:"+num+"rom:"+rom[i]);
+            System.out.println("num/rom:"+num/rom[i]);
+            if( num / rom[i] == 0){
+                if(rom[i] - num <= getPre(rom[i])){
+                    res += getValue(getPre(rom[i]))+""+getValue(rom[i])+"";
+                    num = num - (rom[i] - getPre(rom[i]));
+                }
+                System.out.println("less:"+res);
+            } else if (num / rom[i] > 0){
+                for(int j=1;j<=num / rom[i];j++)
+                    res += getValue(rom[i])+"";
+                num = rem;
+                i--;
+                System.out.println("loop"+res);
+            }
+            i++;
+        }
+        return res;
+    }
+    private int getPre(int n){
+        switch(n){
+            case 1000: return 100;
+            case 500: return 100;
+            case 100: return 10;
+            case 50: return 10;
+            case 10: return 1;
+            case 5: return 1;
+        }
+        return 0;
+    }
+    private char getValue(int n){
+        switch(n){
+            case 1: return 'I';
+            case 5: return 'V';
+            case 10: return 'X';
+            case 50: return 'L';
+            case 100: return 'C';
+            case 500: return 'D';
+            case 1000: return 'M';
+        }
+        return ' ';
+    }
+}
