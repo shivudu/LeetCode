bc89bb5dc93ddcd894c0bdb1d42b7fdf74ae8fd9
shivudu <shivudu.s@gmail.com>
2024-04-24 15:06:43 +0530

    Time: 2 ms (99.41%) | Memory: 48.5 MB (5.16%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,12 +1,37 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
-        for(int n: nums)
-            map.put(n,map.getOrDefault(n,0)+1);
-        List<Integer> res = new ArrayList<>();
-        for(Map.Entry<Integer,Integer> e: map.entrySet())
-            if(e.getValue() > nums.length / 3)
-                res.add(e.getKey());
+       int count1 = 0, count2 = 0;
+       int cand1 = 0, cand2 = 0;
+
+       for(int n: nums){
+        if(count1 == 0 && cand2!=n){
+            count1 = 1;
+            cand1 = n;
+        } else if(count2 == 0 && cand1!=n){
+            count2 = 1;
+            cand2 = n;
+        } else if(cand1 == n){
+            count1++;
+        } else if(cand2 == n){
+            count2++;
+        } else{
+            count1--;
+            count2--;
+        }
+       }
+       List<Integer> res = new ArrayList<>();
+       count1 = 0;
+       count2 = 0;
+       for(int n: nums){
+            if(n == cand1)
+                count1++;
+            else if(n == cand2)
+                count2++;
+       }
+       if(count1 > nums.length /3)
+            res.add(cand1);
+        if(count2 > nums.length / 3)
+            res.add(cand2);
         return res;
     }
 }
