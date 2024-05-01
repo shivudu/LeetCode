2cb517ca1a290e75c0badbd4d28a676821dbd3c3
shivudu <shivudu.s@gmail.com>
2024-05-01 20:28:15 +0530

    Time: 9 ms (98.13%) | Memory: 45.7 MB (20.44%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -4,10 +4,10 @@ class Solution {
             return false;
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
-        for(int i=0;i<word1.length();i++)
-            freq1[word1.charAt(i) -'a']++;
-        for(int i=0;i<word2.length();i++)
-            freq2[word2.charAt(i) -'a']++;
+        for(char c:word1.toCharArray())
+            freq1[c -'a']++;
+        for(char c:word2.toCharArray())
+            freq2[c -'a']++;
         for(int i=0;i<26;i++)
             if((freq1[i] == 0 && freq2[i]!=0) || (freq2[i] == 0 && freq1[i]!=0))
                 return false;
